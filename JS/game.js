5ecf0adfd9b8f1ee7deb3962c9e06289ef923692
kaazigemosterd <6ssl@users.noreply.github.com>
2026-06-12 08:52:26 +0200

    Refactor game UI and add responsive styles
    Rework game page structure and styling for a responsive layout: introduce .game-header, .game-nav, .game-area and .card containers; add themed button classes (mario, luigi, yoshi, tugra) and back-button positioning/active states; add mobile/viewport media queries in CSS. Update global styles.css with additional responsive rules for header, buttons and layout breakpoints. Update game.html to move navigation into a header, add project links/buttons, wrap the score UI in a .card, and add a footer. Wire up navigation in game.js by selecting specific button classes (back, mario, luigi, alisa, website) and attach click handlers; simplify index.js by removing duplicate/unused handlers and keep the play button navigation. Also includes small ARIA/attribute changes to the clicker button and some markup cleanup.

--- JS/game.js
+++ JS/game.js
@@ -1,7 +1,35 @@
-const backButton = document.querySelector('.button-with-icon');
+const backButton = document.querySelector('.back-button');
 if (backButton) {
   backButton.addEventListener('click', () => {
     window.location.href = '../index.html';
+  });
+}
+
+const marioButton = document.querySelector('.button-with-icon.mario');
+if (marioButton) {
+  marioButton.addEventListener('click', () => {
+    location.href = 'https://walter-white-the-creator.github.io/jasper/';
+  });
+}
+
+const alisabutton = document.querySelector('.button-with-icon.alisabutton');
+if (alisabutton) {
+  alisabutton.addEventListener('click', () => {
+    location.href = 'https://alischoolsp.github.io/project-web-dev-2026/index.html';
+  });
+}
+
+const websiteButton = document.querySelector('.button-with-icon.website-link');
+if (websiteButton) {
+  websiteButton.addEventListener('click', () => {
+    location.href = 'https://tyehx.github.io/project-web-dev-2026/';
+  });
+}
+
+const luigiButton = document.querySelector('.button-with-icon.luigi');
+if (luigiButton) {
+  luigiButton.addEventListener('click', () => {
+    location.href = 'https://jellie12.github.io/Project-Web-Dev-2026/';
   });
 }
 
--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,6 @@
-const luigiButton = document.querySelector('.button-with-icon.luigi');
-if (luigiButton) {
-  luigiButton.addEventListener('click', () => {
-    location.href = 'https://jellie12.github.io/Project-Web-Dev-2026/';
-  });
-}
-
 const playButton = document.querySelector('.center-area .button-with-icon');
 if (playButton) {
   playButton.addEventListener('click', () => {
     location.href = 'HTML/game.html';
-  });
-}
-
-const marioButton = document.querySelector('.button-with-icon.mario');
-if (marioButton) {
-  marioButton.addEventListener('click', () => {
-    location.href = 'https://walter-white-the-creator.github.io/jasper/';
   });
 }
